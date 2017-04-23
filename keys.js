4c56645cb954236478438932a24048cbcf07b210
Andy <Andy@Admins-MacBook-Air.local>
2017-04-22 21:36:49 -0400

    commit files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'bHE1NKyPw5Rsp1SywY6waxDkC',
+  consumer_secret: 'IbbjSucgSxNRqUxltMNZDqLmk5EN2JcHg40JjBki7DDjjyIxOQ',
+  access_token_key: '221763076-4h1H1Ld00MIFoGARaN33B9qe4gVlkBnoKZZLWZsL',
+  access_token_secret: '3MbB3146iDC84WhTxPn7H2xOoZ9006nGquJLOLWcGcXo0',
+}
---
+++ liri.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+var keys = require("./keys.js");
+
+var twitterKeys = keys.twitterKeys
+
+console.log(twitterKeys);
