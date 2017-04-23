dbf8f66021673f4af59039cefe9207eb37098364
Andy <Andy@Admins-MacBook-Air.local>
2017-04-22 23:51:27 -0400

    commit progress

--- liri.js
+++ liri.js
@@ -1,14 +1,84 @@
 var fs = require("fs");
 var keys = require("./keys.js");
 var Twitter = require('twitter');
+var spotify = require('spotify');
+var spotify = require('request');
 var twitterKeys = keys.twitterKeys; 
 var client = new Twitter(twitterKeys);
 
+var commandOne = process.argv[2];
+var commandTwo = process.argv[3];
 
-var params = {screen_name: 'nodejs'};
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  }
+
+
+switch (commandOne){
+
+	case ("my-tweets"):
+		twitter()
+		break;
+
+	case ("spotify-this-song"):
+		spotifyFunc()
+		break;
+
+	default: 
+		console.log("Invalid command");
+};
+
+
+
+
+
+
+function twitter(){
+
+	
+	var params = {screen_name: 'APatrick12'};
+
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+		if (!error) {
+    // console.log(JSON.stringify(tweets, null, 3));
+
+    
+    	for (i=0; i< tweets.length; i++){
+    		console.log(tweets[i].text);
+
+    		console.log(tweets[i].created_at);
+   			 }
+		}
+	});
+};
+
+
+
+function spotifyFunc(){
+
+	spotify.search({ type: 'track', query: commandTwo }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 	
+ 	var songObj = data.tracks.items[0];
+ 	
+    // console.log(JSON.stringify(data, null, 3));
+    //artist
+    console.log(songObj.artists[0].name);
+    //song
+    console.log(songObj.name);
+    //link to song
+    console.log(songObj.preview_url);
+    //album
+ 	console.log(songObj.album.name);
+
 });
+
+};
+
+
+
+
+
+
+
